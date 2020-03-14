b167eca7aac955aed9ebef43b8ea37a7e91302f4
Ahmad Noor <ahmad.noor@mobilelive.ca>
2020-03-15 04:31:18 +0500

    add inputBar layout

---
+++ src/layouts/InputBar.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react'
+import Button from '../components/Button'
+import Input from '../components/Input'
+
+export default function InputBar ({ history }) {
+  const [value, setValue] = useState('')
+
+  const handleInput = e => {
+    setValue(e.target.value)
+  }
+
+  const handleClick = () => {
+    history.push(`/${value}`)
+  }
+
+  return (
+    <header className='header'>
+      <div className='container'>
+        <h1 className='heading text-center'>My Github Resume</h1>
+        <div className='card'>
+          <div className='card-body'>
+            <div className='d-flex'>
+              <Input
+                value={value}
+                handleInput={handleInput}
+                label='Github Username'
+              />
+              <Button
+                onHandleClick={handleClick}
+                buttonText='Generate'
+                isDisable={!value ? true : false}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
