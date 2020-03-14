6fb4e750072d0d011bbb7c8be399d5c572d988b1
Ahmad Noor <ahmad.noor@mobilelive.ca>
2020-03-15 04:32:20 +0500

    add main component

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import React, { lazy, Suspense } from 'react'
+import '../public/scss/main.scss'
+import { Switch, BrowserRouter as Router, Route } from 'react-router-dom'
+import ErrorBoundary from './ErrorBoundries'
+
+const SearchBar = lazy(() => import('./layouts/InputBar'))
+const ResumeDetails = lazy(() => import('./layouts/ResumeDetail'))
+
+function App () {
+  return (
+    <Suspense fallback={<h1>Loading...</h1>}>
+      <div id='wrapper'>
+        <Router>
+          <ErrorBoundary>
+            <Switch>
+              <Route exact path='/' component={SearchBar} />
+              <Route
+                path='/:name'
+                component={props => (
+                  <ResumeDetails name={props.match.params.name} />
+                )}
+              />
+            </Switch>
+          </ErrorBoundary>
+        </Router>
+      </div>
+    </Suspense>
+  )
+}
+
+export default App
